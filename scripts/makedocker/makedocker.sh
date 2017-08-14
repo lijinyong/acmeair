63a6f4cf114b9723eb342e5f53647ed868100e00
TankTian <chd.tianming@gmail.com>
2017-08-14 14:37:05 +0800

    和CPE联调需要添加镜像文件

---
+++ scripts/makedocker/makedocker.sh
@@ -0,0 +1,14 @@
+CUR_PATH=$(cd "$(dirname "$0")"; pwd)
+ROOT_PATH="${CUR_PATH}/../../"
+cd "${ROOT_PATH}"
+mvn clean  install -DskipTests  -Phuaweicloud
+cd "${CUR_PATH}"
+cp ./customer/Dockerfile  ${ROOT_PATH}/acmeair-customer-service/target/acmeair/
+cp ./booking/Dockerfile  ${ROOT_PATH}/acmeair-booking-service/target/acmeair/
+cp ./website/Dockerfile   ${ROOT_PATH}/acmeair-website/target/
+cd ${ROOT_PATH}/acmeair-customer-service/target/acmeair/
+docker build -t acmeair-customer-service:3.0.0-SNAPSHOT  . 
+cd  ${ROOT_PATH}/acmeair/acmeair-booking-service/target/acmeair/
+docker build -t acmeair-booking-service:3.0.0-SNAPSHOT .
+cd ${ROOT_PATH}/acmeair/acmeair-website/target/
+docker build -t acmeair-website:3.0.0-SNAPSHOT  .
